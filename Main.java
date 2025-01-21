d0526aa366e1c7495e4a4d2371ccdcbb54243410
SparklePhoenix <kelsey.c@igniterobotics.com>
2025-01-21 17:45:02 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+Scanner sc = new Scanner (System.in);
+String name;
+System.out.println("Hello! What is your name?");
+    name = sc.next();
+    System.out.println("Nice to meet you " + name + "!");
+
+
+
+
+
+  }
+}
